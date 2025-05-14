5c0320638e84bdce9302bc343dacd16ebdafecd7
RDG-lambda <rabanushuber@t-online.de>
2025-05-14 18:34:09 +0200

    Main gecleart

--- sidebarmenu.js
+++
@@ -1,7 +0,0 @@
-function toggleMenu() {
-    var sidebar = document.getElementById("sidebar");
-    var hamburger = document.getElementById("hamburger");
-
-    sidebar.classList.toggle("open");
-    hamburger.classList.toggle("open");
-}
